32c177f7c1035f22687efa5c47b871030c1b5191
Reny Mathew <renykmbloc@gmail.com>
2017-03-07 22:37:08 -0800

    assignment-dom-scripting-animation: completed-corrected

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,16 +2,16 @@ var animatePoints = function() {
  
     var points = document.getElementsByClassName('point');
     
-    var revealPoint = function() {
-        for (var i=0; i < document.getElementsByClassName('point').length; i++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
  
-    revealPoint();
+    for (var i=0; i < points.length; i++){
+        revealPoint(index);
+    }
   
 };
              
